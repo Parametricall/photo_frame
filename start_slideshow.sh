b0562550036048aa52dd6f688f9152de7181921e
Parametricall <22248105+Parametricall@users.noreply.github.com>
2020-10-11 13:35:52 +1000

    small update

--- start_slideshow.sh
+++ start_slideshow.sh
@@ -6,3 +6,8 @@ echo "Time: $(date) ready" >> startup.log
 
 poetry run python slideshow.py
 
+# examples
+#poetry run python slideshow.py slideshow_delay image_directory --LOG_LEVEL LOGLEVEL --INSANE_LOGGING
+#poetry run python slideshow.py 30 /media/usb/images --LOG_LEVEL INFO --INSANE_LOGGING
+#poetry run python slideshow.py --INSANE_LOGGING
+
